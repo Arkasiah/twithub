62c3aaf5201421c834520eb032cfbc5205a6fcd7
Guillaume <guillaume.boyere@hotmail.fr>
2022-09-22 04:28:40 +0200

    log res

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { GetStaticProps } from "next"
+import {GetServerSideProps} from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 // pages/index.tsx
@@ -7,7 +7,7 @@ import prisma from '../lib/prisma';
 
 
 // index.tsx
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
     include: {
